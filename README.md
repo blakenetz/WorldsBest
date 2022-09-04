804f9664f1234bc5b0d3af340092d086cf7e8091
Blake Netzeband <blake.netzeband@gmail.com>
2022-09-03 21:44:13 -0600

    create google maps instance

---
+++ README.md
@@ -0,0 +1,19 @@
+# World's Best
+
+Create google map lists for the world's best restaurants.
+
+## What it does:
+
+1. scrap [world's best restaurant list](https://www.theworlds50best.com/list/1-50)
+2. create a new list in Google maps
+3. add's pins for each entry
+
+## Steps
+
+1. Install deps
+
+```
+$ pip3 install -r requirements.txt
+```
+
+2. Create [Google Maps API keys](https://developers.google.com/maps/get-started)
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import requests
 import re
 import os
+import googlemaps
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
+# env business
 load_dotenv()
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
 
+# initiate beautiful soup
 r = requests.get('https://www.theworlds50best.com/list/1-50')
 soup = BeautifulSoup(r.text, 'html.parser')
-# fetch items
-items = soup.select("div[data-list]:not([data-list*='Individual']) .item")
 
-# generate list of restaurants
+# fetch items and generate meaningful list
 data = []
+items = soup.select("div[data-list]:not([data-list*='Individual']) .item")
 for item in items:
     name = item.find('h2')
     entry = {
@@ -21,7 +24,9 @@ for item in items:
     }
     data.append(entry)
 
+# create google maps list
+gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
 
-# GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
+
 # SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
 # STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
